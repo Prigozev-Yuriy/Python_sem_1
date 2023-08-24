0378bc35558a5f00e20874a3277cb8c1c90dec87
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-08-24 22:18:14 +0300

    finish commit

--- Seminar1.py
+++ Seminar1.py
@@ -3,11 +3,18 @@
 # При решении этой задачи нельзя пользоваться условной инструкцией if и циклами. Input: n = 700 m = 750 Output: 2
 
 # from math import ceil
-#
 # day_trip = input('Введите, сколько километров машина проезжает за день: ')
 # common_path = input('Введите, сколько километров машина должна проехать: ')
+# print (f'Машина проедет {common_path} километров за {str(ceil(int(common_path)/int(day_trip)))} дней')
+
+# print(f'Машина проедет {common_path} километров за {int(common_path) // int(day_trip) + int(bool(int(common_path)%int(day_trip)))} дней')
+
+
+# n = int(input("Введите кол-во киллометров, которое проезжаете за день: "))
+# m = int(input("Введите расстояние: "))
+# print((int(common_path) + int(day_trip) - 1) // int(day_trip))
 #
-# print ('Машина проедет ' + common_path + ' километров за ' + str(ceil(int(common_path)/int(day_trip))) + ' дней')
+
 
 # Задача №3
 # В некоторой школе решили набрать три новых математических класса и оборудовать кабинеты для них новыми партами.
@@ -16,12 +23,15 @@
 # Input: 20 21 22(ввод чисел НЕ в одну строку) Output: 32
 
 # from math import ceil
-#
 # first_grade_students = int(input('Введите число учащихся в первом классе: '))
 # second_grade_students = int(input('Введите число учащихся во втором классе: '))
 # third_grade_students = int(input('Введите число учащихся в третьем классе: '))
+# print(f'Необходимо приобрести {str(ceil(first_grade_students/2) + ceil(second_grade_students/2) + ceil(third_grade_students/2))} '
+#       f'парт')
 #
-# print('Необходимо приобрести ' + str(ceil(first_grade_students/2) + ceil(second_grade_students/2) + ceil(third_grade_students/2)) + ' парт')
+# print(f'Необходимо приобрести '
+#       f'{first_grade_students//2 + first_grade_students % 2 + second_grade_students // 2 + second_grade_students % 2 + third_grade_students //2 + third_grade_students % 2}')
+#
 
 # Задача №5
 # Вагоны в электричке пронумерованы натуральными числами, начиная с 1 (при этом иногда вагоны нумеруются от «головы» поезда,
@@ -34,10 +44,9 @@
 
 # account_number = int(input('Введите номер вагона от головы состава: '))
 # serial_number = int(input('Введите порядковый номер вагона: '))
-# print('Нумерация поезда с головы состава, для определения количества вагонов поезда необходимы дополнительные сведения' if account_number==serial_number
-#       else 'В электричке ' + str(account_number + serial_number - 1) + ' вагонов')
-
-
+# print(f'Нумерация поезда с головы состава, для определения количества вагонов поезда необходимы дополнительные сведения'
+#       if account_number==serial_number
+#       else f'В электричке {(account_number + serial_number - 1)} вагонов')
 
 # Задача №7. Решение в группах Дано натуральное число. Требуется определить, является ли год с данным номером високосным.
 # Если год является високосным, то выведите YES, иначе выведите NO. Напомним, что в соответствии с григорианским календарем,
@@ -45,5 +54,5 @@
 # Input: 2016 Output: YES
 
 
-year = int(input('Введите год: '))
-print('YES' if ((year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)) else 'NO')
+# year = int(input('Введите год: '))
+# print('YES' if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0) else 'NO')
